8240d80b48b8f906ba1a4e1f32f5cc9ecdecd2c7
Shigenaka <mshig19@uw.edu>
2015-12-07 19:53:22 -0800

    updated js

--- js/app.js
+++ js/app.js
@@ -1,42 +1,60 @@
 // Create app
-var myApp = angular.module('myApp', ['ui.router'])
+var myApp = angular.module('myApp', ['ui.router' 'firebase'])
 
 // Configure app
 myApp.config(function($stateProvider) {
 	$stateProvider
-	.state('resume', {
-		url:'/resume',
-		templateUrl: 'templates/resume.html',
-		controller: 'ResumeController',
-	})
-	.state('interests', {
-		url:'/interests',
-		templateUrl: 'templates/interests.html',
-		controller: 'InterestsController',
+	.state('about', {
+		url:'/',
+		templateUrl: 'templates/about.html',
+		controller: 'AboutController',
 	})
 
-	.state('work', {
-		url:'/work',
-		templateUrl: 'templates/work.html',
-		controller: 'WorkController',
+	.state('chatback', {
+		url:'/chatback',
+		templateUrl: 'templates/chatback.html',
+		controller: 'ChatBackController',
+	})
+
+	.state('profile', {
+		url:'/profile',
+		templateUrl: 'templates/profile.html',
+		controller: 'ProfileController',
+	})
+
+	.state('privacy', {
+		url:'/privacy',
+		templateUrl: 'templates/privacy.html',
+		controller: 'PrivacyController',
+	})
+
+	.state('contact', {
+		url:'/contact',
+		templateUrl: 'templates/contact.html',
+		controller: 'ContactController',
 	})
 })
 
 // Landing page controller: define $scope.number as a number
-.controller('ResumeController', function($scope){
-  $scope.resume = "../pics/resume2.jpg";
+.controller('AboutController', function($scope){
+
 })
 
 // About page controller: define $scope.about as a string
-.controller('interestsController', function($scope){
+.controller('ChatBackController', function($scope, $firebaseAuth, $firebaseArray, $firebaseObject){
+
 })
 
 // Content controller: define $scope.url as an image
-.controller('WorkController', function($scope, $http){
-	// parses the json file and scopes the array
-	$http.get("projects.json").success(function(response){
-		$scope.projects = response;
-		console.log($scope.projects);
-	});
+.controller('ProfileController', function($scope, $firebaseAuth, $firebaseArray, $firebaseObject){
+
+})
+
+.controller('PrivacyController', function($scope){
+
+})
+
+.controller('ContactController', function($scope){
+
 })
 
